cc93301e2f958e3ce3babf1e47b05ad74016832e
jfcarocota <jfcarocota@gmail.com>
2021-01-22 22:22:01 -0700

    publish

---
+++ Source/labyrinth/labyrinthGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "labyrinthGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LABYRINTH_API AlabyrinthGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
